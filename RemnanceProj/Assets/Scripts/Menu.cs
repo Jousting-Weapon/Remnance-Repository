98abdfbd19161a9b40a78568031a85158b1c52ce
HetonDH <67690426+HetonDH@users.noreply.github.com>
2021-04-06 20:28:27 -0600

    Screen Settings
    Push screen settings

--- RemnanceProj/Assets/Scripts/Menu.cs
+++ RemnanceProj/Assets/Scripts/Menu.cs
@@ -1,7 +1,10 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.Audio;
+using TMPro;
 
 public class Menu : MonoBehaviour
 {
@@ -10,7 +13,8 @@ public class Menu : MonoBehaviour
     private bool doneFading;
     private AudioSource audioSource;
     public AudioMixerGroup masterMixer;
-
+    public GameObject resolutionDropDown;
+    Resolution[] resolutions;
 
     public void Awake()
     {
@@ -20,6 +24,7 @@ public class Menu : MonoBehaviour
         fadeInTime = 3f;
 
         audioSource = gameObject.AddComponent<AudioSource>();
+
         audioSource.GetComponent<AudioSource>().outputAudioMixerGroup = masterMixer;
     }
 
@@ -28,6 +33,29 @@ public class Menu : MonoBehaviour
         audioSource.clip = DialogueAssets.clip_menuMusic;
         audioSource.loop = true;
         audioSource.Play();
+
+        resolutions = Screen.resolutions;
+
+        resolutionDropDown.GetComponent<TMP_Dropdown>().ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height) 
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        
+        resolutionDropDown.GetComponent<TMP_Dropdown>().AddOptions(options);
+        resolutionDropDown.GetComponent<TMP_Dropdown>().value = currentResolutionIndex;
+        resolutionDropDown.GetComponent<TMP_Dropdown>().RefreshShownValue();
     }
 
     public void FixedUpdate()
@@ -42,6 +70,11 @@ public class Menu : MonoBehaviour
                 doneFading = true;
             }
         }
+    }
+
+    public void SetFullscreen(bool isFullscreen) 
+    {
+        Screen.fullScreen = isFullscreen;
     }
 
 }
